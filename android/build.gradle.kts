957b25f828817ca0c0f249677035972ef3f158ca
nh-hadi <nh.hadi234@gmail.com>
2026-09-12 16:17:19 +0700

    fix(gradle): properly configure subprojects compileSdkVersion to 36

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,11 +3,6 @@ allprojects {
         google()
         mavenCentral()
     }
-    tasks.whenTaskAdded {
-        if (name.contains("check") && name.contains("AarMetadata")) {
-            enabled = false
-        }
-    }
 }
 
 val newBuildDir: Directory =
@@ -19,6 +14,15 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        val android = project.extensions.findByName("android")
+        if (android is com.android.build.gradle.BaseExtension) {
+            android.compileSdkVersion(36)
+        }
+    }
 }
 
 subprojects {
